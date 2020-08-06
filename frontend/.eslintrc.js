e21e95dc143f523f607105b452170d39d73d44bc
MagicaQuartet <dev.dskim@gmail.com>
2020-08-06 15:56:34 +0900

    Create frontend/, backend/
    Vue app을 개발할 frontend/ 디렉토리와 express app을 개발할 backend/
    디렉토리를 생성하고 관련 개발 환경을 설정합니다.
    
    Vue app에서는 다음과 같은 플러그인이 사용됩니다.
    - eslint
    - vue-router
    - vuex
    - axios
    - vuetify

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es6: true
+  },
+  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  parserOptions: {
+    parser: "babel-eslint",
+    ecmaVersion: 6
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-unused-vars": "warn" // monkey-patching for axios plugin
+  }
+};
